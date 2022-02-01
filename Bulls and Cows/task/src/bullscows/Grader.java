169d5e6bff9e1169cfc1db98812eb1fb0caea63b
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-02-01 21:12:39 +0200

    GUI

--- Bulls and Cows/task/src/bullscows/Grader.java
+++ Bulls and Cows/task/src/bullscows/Grader.java
@@ -5,38 +5,33 @@ public class Grader {
         boolean isNotFinished = true;
         int bulls = 0;
         int cows = 0;
-        for (int i = 0; i < secret.length(); i++){
-            if (secret.charAt(i) == attempt.charAt(i)){
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == attempt.charAt(i)) {
                 bulls++;
-            }
-            else if (secret.contains(String.valueOf(attempt.charAt(i)))){
+            } else if (secret.contains(String.valueOf(attempt.charAt(i)))) {
                 cows++;
             }
         }
         StringBuilder printer = new StringBuilder("Grade: ");
-        if (bulls > 0 && cows > 0){
+        if (bulls > 0 && cows > 0) {
             if (bulls == 1 & cows == 1)
                 printer.append(bulls).append(" bull and ").append(cows).append(" cow");
             else
                 printer.append(bulls).append(" bulls and ").append(cows).append(" cows");
-        }
-        else if (bulls > 0 && cows == 0){
+        } else if (bulls > 0 && cows == 0) {
             if (bulls == secret.length() && secret.length() > 1) {
                 printer.append(bulls).append(" bulls\n").append("Congratulations! You guessed the secret code.");
                 isNotFinished = false;
-            }
-            else if (bulls == 1)
+            } else if (bulls == 1)
                 printer.append(bulls).append(" bull");
             else if (bulls < secret.length())
                 printer.append(bulls).append(" bulls");
-        }
-        else if (cows > 0 && bulls == 0){
+        } else if (cows > 0 && bulls == 0) {
             if (cows == 1)
                 printer.append(cows).append(" cow");
             else
                 printer.append(cows).append(" cows");
-        }
-        else if (bulls == 0 && cows ==0){
+        } else if (bulls == 0 && cows == 0) {
             printer.append("None");
         }
         System.out.println(printer);
